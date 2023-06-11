706048eef35be0e690bc5c1db16c4c7e7bbd7b31
YOO <fancy73920@gmail.com>
2023-06-11 16:47:02 +0800

    add data

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ data }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    this.$http.get('https://randomuser.me/api/')
+      .then(res => {
+        this.data = res.data.results[0]
+        // console.log(this.data)
+      })
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
